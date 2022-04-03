a4d5d5c68600eb5dbc0330906600b92e47b0e8a9
redjen8 <seokwoo2525@gmail.com>
2022-04-03 13:29:32 +0900

    20220403 jsw Feat : 무한 루프 및 KeyboardInterrupt 추가

--- hw2/TCP/TCPClient.py
+++ hw2/TCP/TCPClient.py
@@ -3,7 +3,6 @@
 #
 
 from socket import *
-import json
 
 serverName = 'localhost'
 serverPort = 12000
@@ -13,45 +12,57 @@ clientSocket.connect((serverName, serverPort))
 
 print("Client is running on port", clientSocket.getsockname()[1])
 
-print('Please input your client option in integer')
-print('Option 1) Convert text to upper case letters')
-print('Option 2) Ask the server what is my ip and port number')
-print('Option 3) Ask the server how many requests it has served so far')
-print('Option 4) Ask the server how long it has been running since it started')
-option = int(input('Option :: '))
+while True:
+    print('<Menu>')
+    print('Please input your client option in integer')
+    print('Option 1) Convert text to upper case letters')
+    print('Option 2) Ask the server what is my ip and port number')
+    print('Option 3) Ask the server how many requests it has served so far')
+    print('Option 4) Ask the server how long it has been running since it started')
+    try:
+        option = int(input('Option :: '))
 
-if option == 1:
-    # send 'ASK_TXTCONV, msg'
-    # recv ['converted_msg']
-    message_cmd = 'ASK_TXTCONV'
-    message_text = input('Input lowercase sentence: ')
-    clientSocket.send((message_cmd + ',' + message_text).encode())
-    modifiedMessage = clientSocket.recv(2048)
-    print('Reply from server:', modifiedMessage.decode())
+        if option == 1:
+            # send 'ASK_TXTCONV, msg'
+            # recv ['converted_msg']
+            message_cmd = 'ASK_TXTCONV'
+            message_text = input('Input lowercase sentence: ')
+            clientSocket.send((message_cmd + ',' + message_text).encode())
+            modifiedMessage = clientSocket.recv(2048)
+            print('Reply from server:', modifiedMessage.decode())
 
-elif option == 2:
-    # send 'ASK_IP_PORT'
-    # recv [xxx.xxx.xxx.xxx, XXXX] which is my ip and port
-    message = 'ASK_IP_PORT'
-    clientSocket.send(message.encode())
-    modifiedMessage = clientSocket.recv(2048).decode()
-    print('Reply from server: client IP = {}, port = {}', )
+        elif option == 2:
+            # send 'ASK_IP_PORT'
+            # recv [xxx.xxx.xxx.xxx, XXXX] which is my ip and port
+            message = 'ASK_IP_PORT'
+            clientSocket.send(message.encode())
+            modifiedMessage = clientSocket.recv(2048).decode()
+            print('Reply from server: client IP = {}, port = {}', )
 
-elif option == 3:
-    # send 'ASK_REQ_NUM'
-    # recv [xxx] which is count of requests served
-    message = 'ASK_REQ_NUM'
-    clientSocket.send(message.encode())
-    modifiedMessage = clientSocket.recv(2048)
-    print('Reply from server:', modifiedMessage.decode())
+        elif option == 3:
+            # send 'ASK_REQ_NUM'
+            # recv [xxx] which is count of requests served
+            message = 'ASK_REQ_NUM'
+            clientSocket.send(message.encode())
+            modifiedMessage = clientSocket.recv(2048)
+            print('Reply from server:', modifiedMessage.decode())
 
-elif option == 4:
-    # send 'ASK_RUNTIME'
-    # recv [xxx] which is run time of the server since it has been started
-    message = 'ASK_RUNTIME'
-    clientSocket.send(message.encode())
-    modifiedMessage = clientSocket.recv(2048)
-    print('Reply from server:', modifiedMessage.decode())
+        elif option == 4:
+            # send 'ASK_RUNTIME'
+            # recv [xxx] which is run time of the server since it has been started
+            message = 'ASK_RUNTIME'
+            clientSocket.send(message.encode())
+            modifiedMessage = clientSocket.recv(2048)
+            print('Reply from server:', modifiedMessage.decode())
 
+        elif option == 5:
+            print('Bye bye~')
+
+        else:
+            print('Wrong Option Selected. Please Select menu again.')
+
+    except KeyboardInterrupt:
+        print('Bye bye~')
+        break
 
 clientSocket.close()
