52d7cfe96e3e39172b4e272c2c727875ddd3c680
redjen8 <seokwoo2525@gmail.com>
2022-04-03 23:13:29 +0900

    20220403 jsw Feat : 임계 상태 돌입 구분 변수 추가

--- hw2/TCP/TCPServer.py
+++ hw2/TCP/TCPServer.py
@@ -15,32 +15,39 @@ print("Server is ready to receive on port", serverPort)
 while True:
     (connectionSocket, clientAddress) = serverSocket.accept()
     print('Connection request from', clientAddress)
-    try:
-        message = connectionSocket.recv(2048).decode()
-        requestNumber += 1
+    command_able = True
+    while command_able:
+        command_able = False
+        try:
+            message = connectionSocket.recv(2048).decode()
+            requestNumber += 1
 
-        cmd = message.upper()[:11]
-        replyMessage = ''
+            cmd = message.upper()[:11]
+            replyMessage = ''
 
-        if cmd == 'ASK_TXTCONV':
-            replyMessage = message[12:].upper()
-        elif cmd == 'ASK_IP_PORT':
-            replyMessage = clientAddress[0] + ',' + str(clientAddress[1])
-        elif cmd == 'ASK_REQ_NUM':
-            replyMessage = str(requestNumber)
-        elif cmd == 'ASK_RUNTIME':
-            replyMessage = 'runtime'
-        elif cmd == 'ASK_CONNEND':
+            if cmd == 'ASK_TXTCONV':
+                replyMessage = message[12:].upper()
+            elif cmd == 'ASK_IP_PORT':
+                replyMessage = clientAddress[0] + ',' + str(clientAddress[1])
+            elif cmd == 'ASK_REQ_NUM':
+                replyMessage = str(requestNumber)
+            elif cmd == 'ASK_RUNTIME':
+                replyMessage = 'runtime'
+            elif cmd == 'ASK_CONNEND':
+                print('Bye bye~')
+                connectionSocket.close()
+            else:
+                replyMessage = 'Wrong Command'
+
+            connectionSocket.send(replyMessage.encode())
+
+        except KeyboardInterrupt:
             print('Bye bye~')
-            connectionSocket.close()
-        else:
-            replyMessage = 'Wrong Command'
-
-        connectionSocket.send(replyMessage.encode())
-
-    except KeyboardInterrupt:
-        print('Bye bye~')
-        break
+            break
+        except ConnectionAbortedError:
+            print('Bye bye~')
+        
+        command_able = True
 
     connectionSocket.close()
 
