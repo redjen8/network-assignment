fded0d022ca82ffe309780e4b5c0e712af05a8c9
redjen8 <seokwoo2525@gmail.com>
2022-04-04 21:33:28 +0900

    20220404 jsw Feat : UDP 서버 최초 추가

--- hw2/UDP/UDPServer.py
+++ hw2/UDP/UDPServer.py
@@ -3,15 +3,54 @@
 #
 
 from socket import *
+import datetime
+import time
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
+requestNumber = 0
+byeMessage = 'Bye bye~'
 
 print("Server is ready to receive on port", serverPort)
+runTime = time.time()
 
 while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    print('UDP message from', clientAddress)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    try:
+        message, clientAddress = serverSocket.recvfrom(2048)
+        print('Connection requested from', clientAddress)
+
+        message = message.decode()
+        requestNumber += 1
+
+        cmd = message.upper()[:11]
+        replyMessage = ''
+
+        if cmd == 'ASK_TXTCONV':
+            replyMessage = message[12:].upper()
+        elif cmd == 'ASK_IP_PORT':
+            replyMessage = clientAddress[0] + ',' + str(clientAddress[1])
+        elif cmd == 'ASK_REQ_NUM':
+            replyMessage = str(requestNumber)
+        elif cmd == 'ASK_RUNTIME':
+            currentTime = time.time()
+            replyMessage = str(datetime.timedelta(seconds=currentTime-runTime)).split(".")[0]
+        elif cmd == 'ASK_CONNEND':
+            print(byeMessage)
+            command_able = True
+            break
+            
+        else:
+            replyMessage = 'Wrong Command'
+            command_able = True
+            break
+
+        serverSocket.sendto(replyMessage.encode(), clientAddress)
+
+    except KeyboardInterrupt:
+        print(byeMessage)
+        break
+    except ConnectionAbortedError:
+        print(byeMessage)
+        break
+    
