d20fe148436f9eb9deff5b0240f7704d7f19dc94
redjen8 <seokwoo2525@gmail.com>
2022-04-04 21:27:04 +0900

    20220404 jsw Feat : UDP client 최초 추가

--- hw2/UDP/UDPClient.py
+++ hw2/UDP/UDPClient.py
@@ -3,6 +3,7 @@
 #
 
 from socket import *
+import time
 
 serverName = 'nsl2.cau.ac.kr'
 serverPort = 12000
@@ -12,11 +13,81 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 print("Client is running on port", clientSocket.getsockname()[1])
 
-message = input('Input lowercase sentence: ')
+while True:
+    print('\n<Menu>')
+    print('Please input your client option in integer')
+    print('Option 1) Convert text to upper case letters')
+    print('Option 2) Ask the server what is my ip and port number')
+    print('Option 3) Ask the server how many requests it has served so far')
+    print('Option 4) Ask the server how long it has been running since it started')
+    try:
+        option = int(input('Option :: '))
 
-clientSocket.sendto(message.encode(), (serverName, serverPort))
+        if option == 1:
+            # send 'ASK_TXTCONV, msg'
+            # recv ['converted_msg']
+            message_cmd = 'ASK_TXTCONV'
+            message_text = input('Input lowercase sentence: ')
+            clientSocket.sendto((message_cmd + ',' + message_text).encode(), (serverName, serverPort))
+            start_time = time.perf_counter()
+            modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+            elapsed_time = time.perf_counter() - start_time
+            print('Reply from server:', modifiedMessage.decode())
+            print(f'RTT = {elapsed_time*1000:.2f} ms')
 
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print('Reply from server:', modifiedMessage.decode())
+        elif option == 2:
+            # send 'ASK_IP_PORT'
+            # recv [xxx.xxx.xxx.xxx, XXXX] which is my ip and port
+            message = 'ASK_IP_PORT'
+            clientSocket.sendto(message.encode(), (serverName, serverPort))
+            start_time = time.perf_counter()
+            modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+            elapsed_time = time.perf_counter()-start_time
+            modifiedMessage = modifiedMessage.decode()
+            punct_loc = modifiedMessage.find(',')
+            print(f'Reply from server: client IP = {modifiedMessage[:punct_loc]}, port = {modifiedMessage[punct_loc+1:]}')
+            print(f'RTT = {elapsed_time*1000:.2f} ms')
+
+        elif option == 3:
+            # send 'ASK_REQ_NUM'
+            # recv [xxx] which is count of requests served
+            message = 'ASK_REQ_NUM'
+            clientSocket.sendto(message.encode(), (serverName, serverPort))
+            start_time = time.perf_counter()
+            modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+            elapsed_time = time.perf_counter()-start_time
+            modifiedMessage = modifiedMessage.decode()
+            print('Reply from server: requests served = ', modifiedMessage.decode())
+            print(f'RTT = {elapsed_time*1000:.2f} ms')
+
+        elif option == 4:
+            # send 'ASK_RUNTIME'
+            # recv [xxx] which is run time of the server since it has been started
+            message = 'ASK_RUNTIME'
+            clientSocket.sendto(message.encode(), (serverName, serverPort))
+            start_time = time.perf_counter()
+            modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+            elapsed_time = time.perf_counter()-start_time
+            modifiedMessage = modifiedMessage.decode()
+            print('Reply from server: run time = ', modifiedMessage.decode())
+            print(f'RTT = {elapsed_time*1000:.2f} ms')
+
+        elif option == 5:
+            message = 'ASK_CONNEND'
+            clientSocket.sendto(message.encode(), (serverName, serverPort))
+            print('Bye bye~')
+            break
+
+        else:
+            print('Wrong option Selected. Please Select menu again.')
+
+    except KeyboardInterrupt:
+        message = 'ASK_CONNEND'
+        clientSocket.send(message.encode())
+        print('Bye bye~')
+        break
+    
+    except ValueError:
+        print('Wrong Input, Please Input Again.')
 
 clientSocket.close()
