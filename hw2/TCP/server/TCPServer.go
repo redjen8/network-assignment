a2dd9ce4c32beba8d3b60a5a976208a1fb0a045c
redjen8 <seokwoo2525@gmail.com>
2022-04-05 22:52:01 +0900

    20220405 jsw Feat : Ctrl C 감지하는 go routine 추가

--- hw2/TCP/server/TCPServer.go
+++ hw2/TCP/server/TCPServer.go
@@ -8,8 +8,10 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
 	"strconv"
 	"strings"
+	"syscall"
 	"time"
 )
 
@@ -23,34 +25,50 @@ func main() {
 	buffer := make([]byte, 1024)
 	command_able := true
 
-	for command_able {
-		command_able = false
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-signals
+		fmt.Println("Bye bye~")
+		os.Exit(0)
+	}()
+
+	for {
 		conn, _ := listener.Accept()
 		fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
 
-		count, _ := conn.Read(buffer)
-		request_number += 1
+		for command_able {
+			command_able = false
+			count, _ := conn.Read(buffer)
+			request_number += 1
 
-		recv_message := string(buffer[:count])
-		cmd := strings.ToUpper(recv_message[:11])
-
-		switch cmd {
-		case "ASK_TXTCONV":
-			reply_message := strings.ToUpper(recv_message[12:])
-			conn.Write([]byte(reply_message))
-		case "ASK_IP_PORT":
-			reply_message := conn.RemoteAddr().String()
-			conn.Write([]byte(reply_message))
-		case "ASK_REQ_NUM":
-			reply_message := request_number
-			conn.Write([]byte(strconv.Itoa(reply_message)))
-		case "ASK_RUNTIME":
-			reply_message := time.Since(start_time).String()
-			conn.Write([]byte(reply_message))
-		case "ASK_CONNEND":
-			fmt.Println("Bye bye~")
-			conn.Close()
-			os.Exit(0)
+			recv_message := string(buffer[:count])
+			if len(recv_message) == 0 {
+				command_able = true
+				break
+			}
+			cmd := strings.ToUpper(recv_message[:11])
+			fmt.Println(cmd)
+			switch cmd {
+			case "ASK_TXTCONV":
+				reply_message := strings.ToUpper(recv_message[12:])
+				conn.Write([]byte(reply_message))
+			case "ASK_IP_PORT":
+				reply_message := conn.RemoteAddr().String()
+				conn.Write([]byte(reply_message))
+			case "ASK_REQ_NUM":
+				reply_message := request_number
+				conn.Write([]byte(strconv.Itoa(reply_message)))
+			case "ASK_RUNTIME":
+				reply_message := time.Since(start_time).String()
+				conn.Write([]byte(reply_message))
+			case "ASK_CONNEND":
+				fmt.Println("Bye bye~")
+				conn.Close()
+				break
+			}
+			command_able = true
 		}
 	}
 }
