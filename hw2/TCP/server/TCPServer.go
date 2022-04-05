87102e8b59d223f08901fecd51827603df40e881
redjen8 <seokwoo2525@gmail.com>
2022-04-05 15:56:12 +0900

    20220405 jsw Feat : go TCP 서버 최초 추가

--- hw2/TCP/server/TCPServer.go
+++ hw2/TCP/server/TCPServer.go
@@ -4,22 +4,53 @@
 
 package main
 
-import ("bytes"; "fmt"; "net")
+import (
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
 
 func main() {
-    serverPort := "12000"
+	serverPort := "12000"
 
-    listener, _:= net.Listen("tcp", ":" + serverPort)
-    fmt.Printf("Server is ready to receive on port %s\n", serverPort)
+	listener, _ := net.Listen("tcp", ":"+serverPort)
+	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
+	start_time := time.Now()
+	request_number := 0
+	buffer := make([]byte, 1024)
+	command_able := true
 
-    buffer := make([]byte, 1024)
+	for command_able {
+		command_able = false
+		conn, _ := listener.Accept()
+		fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
 
-    for {
-        conn, _:= listener.Accept()
-        fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
-        count, _ := conn.Read(buffer)
-        conn.Write(bytes.ToUpper(buffer[:count]))
-        conn.Close()
-    }
+		count, _ := conn.Read(buffer)
+		request_number += 1
+
+		recv_message := string(buffer[:count])
+		cmd := strings.ToUpper(recv_message[:11])
+
+		switch cmd {
+		case "ASK_TXTCONV":
+			reply_message := strings.ToUpper(recv_message[12:])
+			conn.Write([]byte(reply_message))
+		case "ASK_IP_PORT":
+			reply_message := conn.RemoteAddr().String()
+			conn.Write([]byte(reply_message))
+		case "ASK_REQ_NUM":
+			reply_message := request_number
+			conn.Write([]byte(strconv.Itoa(reply_message)))
+		case "ASK_RUNTIME":
+			reply_message := time.Since(start_time).String()
+			conn.Write([]byte(reply_message))
+		case "ASK_CONNEND":
+			fmt.Println("Bye bye~")
+			conn.Close()
+			os.Exit(0)
+		}
+	}
 }
-
