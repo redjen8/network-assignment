7f336e9b723a6a5415e2911afa472e9028e3efb9
redjen8 <seokwoo2525@gmail.com>
2022-04-06 00:29:20 +0900

    20220406 jsw Feat : go UDP client 최초 추가

--- hw2/UDP/client/UDPClient.go
+++ hw2/UDP/client/UDPClient.go
@@ -4,27 +4,74 @@
 
 package main
 
-import ("bufio"; "fmt"; "net"; "os";)
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
 
 func main() {
-    serverName := "nsl2.cau.ac.kr"
-    serverPort := "12000"
+	serverName := "localhost"
+	serverPort := "12000"
 
-    pconn, _:= net.ListenPacket("udp", ":")
+	pconn, _ := net.ListenPacket("udp", ":")
 
-    localAddr := pconn.LocalAddr().(*net.UDPAddr)
-    fmt.Printf("Client is running on port %d\n", localAddr.Port)
+	localAddr := pconn.LocalAddr().(*net.UDPAddr)
+	fmt.Printf("Client is running on port %d\n", localAddr.Port)
 
-    fmt.Printf("Input lowercase sentence: ")
-    input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	for {
+		fmt.Println("\n<Menu>")
+		fmt.Println("Please input your client option in integer")
+		fmt.Println("Option 1) Convert text to upper case letters")
+		fmt.Println("Option 2) Ask the server what is my ip and port number")
+		fmt.Println("Option 3) Ask the server how many requests it has served so far")
+		fmt.Println("Option 4) Ask the server how long it has been running since it started")
+		fmt.Print("Option :: ")
 
-    server_addr, _ := net.ResolveUDPAddr("udp", serverName+":"+serverPort)
-    pconn.WriteTo([]byte(input), server_addr)
+		var user_option int
+		fmt.Scanln(&user_option)
 
-    buffer := make([]byte, 1024)
-    pconn.ReadFrom(buffer)
-    fmt.Printf("Reply from server: %s", string(buffer))
+		switch user_option {
+		case 1:
+			var message_cmd, message_text string = "ASK_TXTCONV", ""
+			fmt.Print("Input lowercase sentence: ")
+			fmt.Scanln(&message_text)
+			server_addr, _ := net.ResolveUDPAddr("udp", serverName+":"+serverPort)
+			pconn.WriteTo([]byte(message_cmd+", "+message_text), server_addr)
+			start_time := time.Now()
+			buffer := make([]byte, 1024)
+			pconn.ReadFrom(buffer)
+			elapsed := time.Since(start_time)
+			fmt.Printf("Reply from server: %s\n", string(buffer))
+			fmt.Printf("RTT = %d ms\n", elapsed.Milliseconds())
+			pconn.Close()
+		case 2:
+			var message_cmd string = "ASK_IP_PORT"
+			pconn.WriteTo([]byte(message_cmd))
+			start_time := time.Now()
+			buffer := make([]byte, 1024)
+			pconn.ReadFrom(buffer)
+			elapsed := time.Since(start_time)
+			punct_loc := strings.Index(string(buffer), ",")
+			fmt.Printf("Reply from server: client IP = %s, port = %s\n", string(buffer)[:punct_loc], string(buffer[punct_loc+1:]))
+			fmt.Printf("RTT = %d\n", elapsed.Milliseconds())
+		case 3:
+		case 4:
+		case 5:
 
-    pconn.Close()
+		}
+	}
+
+	fmt.Printf("Input lowercase sentence: ")
+	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	server_addr, _ := net.ResolveUDPAddr("udp", serverName+":"+serverPort)
+	pconn.WriteTo([]byte(input), server_addr)
+
+	buffer := make([]byte, 1024)
+	pconn.ReadFrom(buffer)
+	fmt.Printf("Reply from server: %s", string(buffer))
 }
-
