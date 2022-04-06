d79ff61b8490a20769b3a5469d34ea51cfd22428
redjen8 <seokwoo2525@gmail.com>
2022-04-06 22:54:33 +0900

    20220406 jsw Feat : 옵션 3, 4, 5 추가 및 셧다운 훅 추가

--- hw2/TCP/client/EasyTCPClient.java
+++ hw2/TCP/client/EasyTCPClient.java
@@ -3,15 +3,18 @@ package hw2.TCP.client;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+import java.util.NoSuchElementException;
 import java.io.*;
 
-public class EasyTCPClient {
+public class EasyTCPClient{
 
     private static final String SERVER_NAME = "localhost";
     private static final int SERVER_PORT = 22848;
-    
+
     public static void main(String[] args) {
         Socket socket = null;
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> { System.out.println("Bye bye~"); }));
 
         try {
             socket = new Socket();
@@ -66,10 +69,40 @@ public class EasyTCPClient {
                     sc.close();
                     break;
                 case 3:
+                    messageCmd = "ASK_REQ_NUM";
+                    os.write(messageCmd.getBytes());
+                    os.flush();
+                    inputBuffer = new byte[1024];
+                    readByteCount = is.read(inputBuffer);
+                    recvMessage = new String(inputBuffer, 0, readByteCount, StandardCharsets.UTF_8);
+                    System.out.println("Reply from server : "+ recvMessage);
+                    is.close();
+                    os.close();
+                    socket.close();
+                    sc.close();
                     break;
                 case 4:
+                    messageCmd = "ASK_RUNTIME";
+                    os.write(messageCmd.getBytes());
+                    os.flush();
+                    inputBuffer = new byte[1024];
+                    readByteCount = is.read(inputBuffer);
+                    recvMessage = new String(inputBuffer, 0, readByteCount, StandardCharsets.UTF_8);
+                    System.out.println("Reply from server : requests served = "+ recvMessage);
+                    is.close();
+                    os.close();
+                    socket.close();
+                    sc.close();
                     break;
                 case 5:
+                    messageCmd = "ASK_CONNEND";
+                    os.write(messageCmd.getBytes());
+                    os.flush();
+
+                    is.close();
+                    os.close();
+                    socket.close();
+                    sc.close();
                     break;
                 default:
                     break;
@@ -79,6 +112,14 @@ public class EasyTCPClient {
         catch (IOException e) {
             e.printStackTrace();
         }
+        catch (NoSuchElementException e) {
+            try {
+                socket.close();
+            }
+            catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
         if (!socket.isClosed()) {
             try {
                 socket.close();
@@ -87,7 +128,5 @@ public class EasyTCPClient {
                 e.printStackTrace();
             }
         }
-        
-
     }
 }
