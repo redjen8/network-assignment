aef11e766101f06a7cad0ed2664936c0e247059e
redjen8 <seokwoo2525@gmail.com>
2022-04-06 22:41:49 +0900

    20220406 jsw Feat : java TCP client 옵션 1, 2 추가

--- hw2/TCP/client/EasyTCPClient.java
+++ hw2/TCP/client/EasyTCPClient.java
@@ -1,6 +1,7 @@
 package hw2.TCP.client;
 
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 import java.io.*;
 
@@ -15,26 +16,65 @@ public class EasyTCPClient {
         try {
             socket = new Socket();
             socket.connect(new InetSocketAddress(SERVER_NAME, SERVER_PORT));
-            byte[] bytes = null;
-            String message = null;
+            byte[] inputBuffer = null;
+            String recvMessage = null;
             OutputStream os = socket.getOutputStream();
+            InputStream is = socket.getInputStream();
 
             Scanner sc = new Scanner(System.in);
-
             
-            message = "ASK_IP_PORT";
-            bytes = message.getBytes("UTF-8");
-            os.write(bytes);
-            os.flush();
-
-            InputStream is = socket.getInputStream();
-            bytes = new byte[1024];
-            int readByteCount = is.read(bytes);
-            message = new String(bytes, 0, readByteCount, "UTF-8");
-            System.out.println("message : " + message);
-            os.close();
-            is.close();
-            socket.close();
+            System.out.println("\n<Menu>");
+            System.out.println("Please input your client option in integer");
+            System.out.println("Option 1) Convert text to upper case letters");
+            System.out.println("Option 2) Ask the server what is my ip and port number");
+            System.out.println("Option 3) Ask the server how many requests it has served so far");
+            System.out.println("Option 4) Ask the server how long it has been running since it started");
+            System.out.println("Option 5) Exit");
+            System.out.print("Option :: ");
+            
+            int userOption = sc.nextInt();
+            sc.nextLine();
+            String messageCmd = null;
+            int readByteCount;
+            switch (userOption) {
+                case 1:
+                    messageCmd = "ASK_TXTCONV";
+                    System.out.print("Input lowercase sentence: ");
+                    String messageText = sc.nextLine();
+                    os.write((messageCmd + "," + messageText).getBytes());
+                    os.flush();
+                    inputBuffer = new byte[1024];
+                    readByteCount = is.read(inputBuffer);
+                    recvMessage = new String(inputBuffer, 0, readByteCount, StandardCharsets.UTF_8);
+                    System.out.println("Reply from server : "+ recvMessage);
+                    is.close();
+                    os.close();
+                    socket.close();
+                    sc.close();
+                    break;
+                case 2:
+                    messageCmd = "ASK_IP_PORT";
+                    os.write(messageCmd.getBytes());
+                    os.flush();
+                    inputBuffer = new byte[1024];
+                    readByteCount = is.read(inputBuffer);
+                    recvMessage = new String(inputBuffer, 0, readByteCount, StandardCharsets.UTF_8);
+                    System.out.println("Reply from server : "+ recvMessage);
+                    is.close();
+                    os.close();
+                    socket.close();
+                    sc.close();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    break;
+            }
+            
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -47,6 +87,7 @@ public class EasyTCPClient {
                 e.printStackTrace();
             }
         }
+        
 
     }
 }
