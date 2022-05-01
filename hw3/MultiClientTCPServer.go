e3d6befab43928f2bb72662c3fe34b2647d36532
redjen8 <seokwoo2525@gmail.com>
2022-05-01 15:59:31 +0900

    20220501 jsw Feat : go routine 함수 기명으로 전환

--- hw3/MultiClientTCPServer.go
+++ hw3/MultiClientTCPServer.go
@@ -16,6 +16,44 @@ import (
 	"time"
 )
 
+func connection_handle(conn net.Conn, name int, start_time time.Time, request_number int, buffer []byte) {
+	conn_flag := true
+	for conn_flag {
+		count, _ := conn.Read(buffer)
+		request_number += 1
+
+		recv_message := string(buffer[:count])
+		if len(recv_message) == 0 {
+			fmt.Println("Command Length 0, Invalid!")
+		}
+		cmd := strings.ToUpper(recv_message[:11])
+
+		switch cmd {
+		case "ASK_TXTCONV":
+			reply_message := strings.ToUpper(recv_message[12:])
+			conn.Write([]byte(reply_message))
+			fmt.Printf("Command %s Executed From %s.\n", strconv.Itoa(request_number), conn.RemoteAddr().String())
+		case "ASK_IP_PORT":
+			reply_message := conn.RemoteAddr().String()
+			conn.Write([]byte(reply_message))
+			fmt.Printf("Command %s Executed From %s.\n", strconv.Itoa(request_number), conn.RemoteAddr().String())
+		case "ASK_REQ_NUM":
+			reply_message := request_number
+			conn.Write([]byte(strconv.Itoa(reply_message)))
+			fmt.Printf("Command %s Executed From %s.\n", strconv.Itoa(request_number), conn.RemoteAddr().String())
+		case "ASK_RUNTIME":
+			reply_message := time.Time{}.Add(time.Since(start_time))
+			conn.Write([]byte(reply_message.Format("15:04:05")))
+			fmt.Printf("Command %s Executed From %s.\n", strconv.Itoa(request_number), conn.RemoteAddr().String())
+		case "ASK_CONNEND":
+			fmt.Println("Bye bye~")
+			conn.Close()
+			conn_flag = false
+			break
+		}
+	}
+}
+
 func main() {
 	serverPort := "22848"
 
@@ -23,6 +61,7 @@ func main() {
 	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
 	start_time := time.Now()
 	request_number := 0
+	connection_number := 0
 	buffer := make([]byte, 1024)
 
 	signals := make(chan os.Signal, 1)
@@ -37,43 +76,7 @@ func main() {
 	for {
 		conn, _ := listener.Accept()
 		fmt.Printf("Connection request from %s\n", conn.RemoteAddr().String())
-
-		go func() {
-			conn_flag := true
-			for conn_flag {
-				count, _ := conn.Read(buffer)
-				request_number += 1
-
-				recv_message := string(buffer[:count])
-				if len(recv_message) == 0 {
-					fmt.Println("Command Length 0, Invalid!")
-				}
-				cmd := strings.ToUpper(recv_message[:11])
-
-				switch cmd {
-				case "ASK_TXTCONV":
-					reply_message := strings.ToUpper(recv_message[12:])
-					conn.Write([]byte(reply_message))
-					fmt.Printf("Command %s Executed From %s.\n", strconv.Itoa(request_number), conn.RemoteAddr().String())
-				case "ASK_IP_PORT":
-					reply_message := conn.RemoteAddr().String()
-					conn.Write([]byte(reply_message))
-					fmt.Printf("Command %s Executed From %s.\n", strconv.Itoa(request_number), conn.RemoteAddr().String())
-				case "ASK_REQ_NUM":
-					reply_message := request_number
-					conn.Write([]byte(strconv.Itoa(reply_message)))
-					fmt.Printf("Command %s Executed From %s.\n", strconv.Itoa(request_number), conn.RemoteAddr().String())
-				case "ASK_RUNTIME":
-					reply_message := time.Time{}.Add(time.Since(start_time))
-					conn.Write([]byte(reply_message.Format("15:04:05")))
-					fmt.Printf("Command %s Executed From %s.\n", strconv.Itoa(request_number), conn.RemoteAddr().String())
-				case "ASK_CONNEND":
-					fmt.Println("Bye bye~")
-					conn.Close()
-					conn_flag = false
-					break
-				}
-			}
-		}()
+		connection_number += 1
+		go connection_handle(conn, connection_number, start_time, request_number, buffer)
 	}
 }
