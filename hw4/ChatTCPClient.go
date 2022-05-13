16dcc73b954148284cabd2dc8a3e2214e4405748
redjen8 <seokwoo2525@gmail.com>
2022-05-13 23:29:48 +0900

    20220513 jsw Feat : 클라이언트 및 서버 닉네임 연동 추가

--- hw4/ChatTCPClient.go
+++ hw4/ChatTCPClient.go
@@ -10,9 +10,15 @@ import (
 	"syscall"
 )
 
-func sendTCPData(command_int int, conn net.Conn) {
-	buf := make([]byte, 4)
+func sendTCPData(command_int int, message string, conn net.Conn) {
+	buf := make([]byte, 1024)
 	binary.LittleEndian.PutUint32(buf, uint32(command_int))
+	if message != "" {
+		copy(buf[1:], message)
+		fmt.Printf(string(buf[:]))
+	} else {
+		fmt.Printf(string(buf[:]))
+	}
 	conn.Write(buf)
 }
 
@@ -35,6 +41,9 @@ func main() {
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 
 	conn, _ := net.Dial("tcp", serverName+":"+serverPort)
+	sendTCPData(0, nickname, conn)
+
+	conn.Write([]byte(nickname))
 
 	localAddr := conn.LocalAddr().(*net.TCPAddr)
 	fmt.Printf("Welcome %s to CAU network classroom at %s:%s. Client is running on port %d.\n", nickname, serverName, serverPort, localAddr.Port)
@@ -47,31 +56,25 @@ func main() {
 
 	go readServerUpdate(conn)
 
-	for {
+	clientFlag := true
+	for clientFlag {
 		var user_input string
 		fmt.Scanln(&user_input)
 		slice := strings.Split(user_input, " ")
 
 		switch slice[0] {
 		case "\\list":
-			command_int := 1
-			sendTCPData(command_int, conn)
+			sendTCPData(1, "", conn)
 		case "\\dm":
-			command_int := 2
-			sendTCPData(command_int, conn)
+			sendTCPData(2, "message", conn)
 		case "\\exit":
-			command_int := 3
-			sendTCPData(command_int, conn)
+			sendTCPData(3, "", conn)
+			clientFlag = false
 		case "\\ver":
-			command_int := 4
-			sendTCPData(command_int, conn)
+			sendTCPData(4, "", conn)
 		case "\\rtt":
-			command_int := 5
-			sendTCPData(command_int, conn)
-		default:
-			command_int := 0
-			sendTCPData(command_int, conn)
+			sendTCPData(5, "", conn)
 		}
-		break
+		sendTCPData(99, "some message", conn)
 	}
 }
--- hw4/ChatTCPServer.go
+++ hw4/ChatTCPServer.go
@@ -1,5 +1,88 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+)
+
+var clientConnInfoMap = map[string]string{}
+var clientConnMap = map[string]net.Conn{}
+
+func connection_handle(conn net.Conn) {
+	conn_flag := true
+	buffer := make([]byte, 1024)
+
+	for conn_flag {
+		count, _ := conn.Read(buffer)
+		user_command := buffer[0]
+		switch user_command {
+		case 0:
+			fmt.Printf("Welcome User : %s!\n", buffer[1])
+		case 1:
+			// \list command
+			fmt.Printf("Welcome User : %s!\n", buffer[1])
+		case 2:
+			// \dm command
+			fmt.Printf("")
+		case 3:
+			// \exit command
+			fmt.Printf("")
+			conn_flag = false
+		case 4:
+			// \ver command
+			fmt.Printf("")
+		case 5:
+			// \rtt command
+			fmt.Printf("")
+		case 99:
+			// without any command, default chat
+			fmt.Printf("")
+		}
+		fmt.Printf("command %d : %s from %s\n", user_command, buffer[:count], conn.RemoteAddr().String())
+	}
+}
+
 func main() {
 
+	serverPort := "22848"
+
+	listener, _ := net.Listen("tcp", ":"+serverPort)
+	fmt.Printf("Server is ready to receive on port %s\n", serverPort)
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-signals
+		fmt.Println("Bye bye~")
+		os.Exit(0)
+	}()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		defer conn.Close()
+		buffer := make([]byte, 1024)
+		count, _ := conn.Read(buffer)
+		userNickname := string(buffer[:count])
+		fmt.Printf("User nickname : %s\n", userNickname)
+		fmt.Printf("User addr : %s\n", conn.RemoteAddr())
+		if existClientInfo, isAlreadyExists := clientConnInfoMap[userNickname]; isAlreadyExists {
+			fmt.Printf("User nickname conflict. User Info : %s\n", existClientInfo[0])
+			_, err = conn.Write([]byte(strconv.Itoa(-1)))
+			if err != nil {
+				continue
+			}
+		} else {
+			clientConnInfoMap[userNickname] = conn.RemoteAddr().String()
+			clientConnMap[userNickname] = conn
+			go connection_handle(conn)
+		}
+	}
 }
