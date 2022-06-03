bdec4c73e9ba694df2f1d75d4ef6beb47520b085
redjen8 <seokwoo2525@gmail.com>
2022-06-03 22:59:58 +0900

    20220603 jsw Feat : 랑데부 서버 1차 구현 완료

--- hw5/P2POmokServer.go
+++ hw5/P2POmokServer.go
@@ -10,44 +10,47 @@ import (
 	"syscall"
 )
 
-var clientConnInfoMap = map[string]string{}
-var clientUDPPortMap = map[string]int{}
-var clientConnMap = map[string]net.Conn{}
+var clientConnInfoMap = map[string]string{} // stores ip address of client
+var clientUDPPortMap = map[string]int{}     // stores opened udp port of client
+var clientConnMap = map[string]net.Conn{}   // stores actual tcp connection of client
 
 func connectionHandle(userNickname string) {
 	userUDPPort := clientUDPPortMap[userNickname]
+	var opponentNickname string
+
 	if len(clientConnMap) == 1 {
 		// first player connected to server. waits..
-		fmt.Println(userNickname + " joined from " + clientConnInfoMap[userNickname] + ". UDP port " + strconv.Itoa(userUDPPort))
+		fmt.Println(userNickname + " joined from " + clientConnInfoMap[userNickname] + ". UDP port " + strconv.Itoa(userUDPPort) + ".")
 		fmt.Println("1 user connected, Waiting for another")
 	} else if len(clientConnMap) == 2 {
 		// second player connected to server, starts game
 		strconv.Itoa(clientUDPPortMap[userNickname])
-		fmt.Println(userNickname + " joined from " + clientConnInfoMap[userNickname] + ". UDP port " + strconv.Itoa(userUDPPort))
+		fmt.Println(userNickname + " joined from " + clientConnInfoMap[userNickname] + ". UDP port " + strconv.Itoa(userUDPPort) + ".")
 
-		var opponentNickname string
-		for key, value := range clientConnInfoMap {
+		for key, _ := range clientConnInfoMap {
 			if strings.Compare(userNickname, key) != 0 {
-				opponentNickname = value
+				opponentNickname = key
 			}
 		}
 
 		fmt.Println("2 user connected, notifying " + opponentNickname + " and " + userNickname)
 		playerConnection := clientConnMap[userNickname]
 		opponentConnection := clientConnMap[opponentNickname]
-		opponentMessage := ""
+		opponentMessage := "0" + userNickname + "." + clientConnInfoMap[userNickname] + ":" + strconv.Itoa(clientUDPPortMap[userNickname])
 		opponentConnection.Write([]byte(opponentMessage))
 
-		playerMessage := ""
+		// first player came into server plays first
+		playerMessage := "1" + opponentNickname + "." + clientConnInfoMap[opponentNickname] + ":" + strconv.Itoa(clientUDPPortMap[opponentNickname])
 		playerConnection.Write([]byte(playerMessage))
 
 		delete(clientConnMap, userNickname)
-		delete(clientConnMap, opponentNickname)
 		delete(clientUDPPortMap, userNickname)
-		delete(clientUDPPortMap, opponentNickname)
 		delete(clientConnInfoMap, userNickname)
+
+		delete(clientConnMap, opponentNickname)
+		delete(clientUDPPortMap, opponentNickname)
 		delete(clientConnInfoMap, opponentNickname)
-		fmt.Println(opponentNickname + " and " + userNickname + "disconnected.")
+		fmt.Println(opponentNickname + " and " + userNickname + " disconnected.")
 	}
 }
 
@@ -62,7 +65,7 @@ func main() {
 
 	go func() {
 		<-signals
-		fmt.Println("gg~")
+		fmt.Println("bye bye~")
 		os.Exit(0)
 	}()
 
@@ -77,11 +80,19 @@ func main() {
 		count, _ := conn.Read(buffer)
 
 		readStr := string(buffer[:count])
-		splitIndex := strings.Index(readStr, ".")
+		splitIndex := strings.Index(readStr, ":")
 		userNickname := readStr[:splitIndex]
 		userUDPPort := readStr[splitIndex+1:]
 
-		clientConnInfoMap[userNickname] = conn.RemoteAddr().String()
+		if existClientInfo, isAlreadyExists := clientConnInfoMap[userNickname]; isAlreadyExists {
+			fmt.Printf("User nickname conflict. User Info : %s\n", existClientInfo)
+			_, err = conn.Write([]byte("that nickname is already used by another user. cannot connect"))
+			if err != nil {
+				continue
+			}
+		}
+		ipAddrIdx := strings.LastIndex(conn.RemoteAddr().String(), ":")
+		clientConnInfoMap[userNickname] = conn.RemoteAddr().String()[:ipAddrIdx]
 		clientUDPPortMap[userNickname], _ = strconv.Atoi(userUDPPort)
 		clientConnMap[userNickname] = conn
 		go connectionHandle(userNickname)
