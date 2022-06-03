03fe316b7f16899c3bdd3f062e8f9882ad0a47bb
redjen8 <seokwoo2525@gmail.com>
2022-06-03 16:20:56 +0900

    20220603 jsw Feat : 서버 connectionHandle 함수 작성

--- hw5/P2POmokServer.go
+++ hw5/P2POmokServer.go
@@ -5,15 +5,48 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 )
 
 var clientConnInfoMap = map[string]string{}
+var clientUDPPortMap = map[string]int{}
 var clientConnMap = map[string]net.Conn{}
 
 func connectionHandle(userNickname string) {
+	userUDPPort := clientUDPPortMap[userNickname]
+	if len(clientConnMap) == 1 {
+		// first player connected to server. waits..
+		fmt.Println(userNickname + " joined from " + clientConnInfoMap[userNickname] + ". UDP port " + strconv.Itoa(userUDPPort))
+		fmt.Println("1 user connected, Waiting for another")
+	} else if len(clientConnMap) == 2 {
+		// second player connected to server, starts game
+		strconv.Itoa(clientUDPPortMap[userNickname])
+		fmt.Println(userNickname + " joined from " + clientConnInfoMap[userNickname] + ". UDP port " + strconv.Itoa(userUDPPort))
 
+		var opponentNickname string
+		for key, value := range clientConnInfoMap {
+			if strings.Compare(userNickname, key) != 0 {
+				opponentNickname = value
+			}
+		}
+
+		fmt.Println("2 user connected, notifying " + opponentNickname + " and " + userNickname)
+		playerConnection := clientConnMap[userNickname]
+		opponentConnection := clientConnMap[opponentNickname]
+		opponentMessage := ""
+		opponentConnection.Write([]byte(opponentMessage))
+
+		playerMessage := ""
+		playerConnection.Write([]byte(playerMessage))
+
+		delete(clientConnMap, userNickname)
+		delete(clientConnMap, opponentNickname)
+		delete(clientConnInfoMap, userNickname)
+		delete(clientConnInfoMap, opponentNickname)
+		fmt.Println(opponentNickname + " and " + userNickname + "disconnected.")
+	}
 }
 
 func main() {
